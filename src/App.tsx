399ac5d52538c4fb34912a4f179a3d518bc7cccc
Ushakov341 <ushakov341@gmail.com>
2025-07-14 14:41:36 +0200

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import './App.css';
+import Input from './components/Input';
 
 const App = () => {
   return (
     <>
-      {/* <Input
+      <Input
         labelText="Имя пользователя"
         id="user_name"
         type="text"
         placeholder="Введите ваше имя"
         className="field"
-      /> */}
+      />
+        <Input
+        labelText="Email"
+        id="email"
+        type="email"
+        placeholder="Введите ваш email"
+        className="field"
+      />
+        <Input
+        labelText="Пароль"
+        id="password"
+        type="password"
+        placeholder="Введите ваш пароль"
+        className="field"
+      />
     </>
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+interface InputProps{
+  id: string;
+  labelText?: string;
+  className?: string;
+  type?: string;
+  placeholder?: string;
+}
+
+function Input(props: InputProps) {
+    return (
+      <div className={props.className ? props.className : ""}>
+        {props.labelText ? (
+          <label htmlFor={props.id}>{props.labelText}</label>
+        ) : null}
+        <input
+          id={props.id}
+          type={props.type ? props.type : "text"}
+          placeholder={props.placeholder ? props.placeholder : ""}
+        />
+      </div>
+    );
+  }
+  
+  export default Input;
