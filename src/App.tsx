03a7056a71321fa77a92c527228c1645f7d4f78a
Ushakov341 <git@stackblitz.com>
2025-07-14 14:13:06 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      {/* <Input
+        labelText="Имя пользователя"
+        id="user_name"
+        type="text"
+        placeholder="Введите ваше имя"
+        className="field"
+      /> */}
+    </>
+  );
+};
+
+export default App;
